1c6a89d7218adcbb437d51ad84d3c04f01766a9b
gaogao <gaoyh@cimu.com.cn>
2015-12-08 13:11:08 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './src/components/AddTodo'
 
-render(
-  <App />,
-  document.getElementById("root")
-    )
+setTimeout(
+  function(){
+    render(
+      <App />,
+      document.getElementById("root")
+        )
+  },
+  2000
+)
